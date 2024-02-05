2cb1cb74224beed37500b1d1fbf9a9ee8b987b67
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2024-02-05 18:45:26 +0530

    download error fixed for ffmpeg

--- helper/download.py
+++ helper/download.py
@@ -33,6 +33,10 @@ def download_and_extract_binary(binary_info):
         print(f"{binary_info['ZipName']} is already present.")
         return
 
+    # Create the binaries directory if it doesn't exist
+    binaries_dir = binary_location.parent
+    binaries_dir.mkdir(parents=True, exist_ok=True)
+
     # Create the downloads/temp directory if it doesn't exist
     temp_dir = Path(binary_info['UnzipFolderLocation'])
     temp_dir.mkdir(parents=True, exist_ok=True)
@@ -46,19 +50,17 @@ def download_and_extract_binary(binary_info):
         # Get the list of files in the zip
         zip_file_contents = zip_ref.namelist()
 
-        # Find the first file with the binary name in the path
-        extracted_binary_path = next(
-            (f for f in zip_file_contents if binary_info['ZipName'] in f), None)
+        # Identify the actual executable file within the extracted folder
+        executable_file = next((f for f in zip_file_contents if f.endswith('.exe')), None)
 
-        # If found, extract the binary
-        if extracted_binary_path:
-            zip_ref.extract(extracted_binary_path,
-                            binary_info['UnzipFolderLocation'])
+        # If found, extract and move the executable file to the binaries folder
+        if executable_file:
+            zip_ref.extract(executable_file, binary_info['UnzipFolderLocation'])
             extracted_binary_path = Path(
-                binary_info['UnzipFolderLocation']) / extracted_binary_path
+                binary_info['UnzipFolderLocation']) / executable_file
             os.rename(extracted_binary_path, binary_info['BinaryLocation'])
             print(
                 f"{binary_info['ZipName']} downloaded and extracted successfully.")
         else:
             print(
-                f"Error: {binary_info['ZipName']} not found in the zip file.")
+                f"Error: Executable file not found in the zip file for {binary_info['ZipName']}.")
